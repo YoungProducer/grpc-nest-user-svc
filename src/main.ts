2f1060174134199bd5d76689fa229aa2300ba4d9
Oleksandr Bezrukov <sashabezrukovownmail@gmail.com>
2023-05-12 10:56:37 +0300

    Now that's a grpc app, added validation pipe.

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,30 @@
 import { NestFactory } from '@nestjs/core';
+import { INestMicroservice, ValidationPipe } from '@nestjs/common';
+import { Transport } from '@nestjs/microservices';
+import { join } from 'node:path';
+
 import { AppModule } from './app.module';
+import { protobufPackage } from './proto/user.pb';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app: INestMicroservice = await NestFactory.createMicroservice(
+    AppModule,
+    {
+      transport: Transport.GRPC,
+      options: {
+        url: 'localhost:50052',
+        package: protobufPackage,
+        protoPath: join(
+          'node_modules/grpc-nest-proto/proto/event-broker.proto',
+        ),
+      },
+      bufferLogs: true,
+    },
+  );
+
+  app.useGlobalPipes(new ValidationPipe({ transform: true, whitelist: true }));
+
+  await app.listen();
 }
+
 bootstrap();
